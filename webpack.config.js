987aaeb08cc4c72d458e826c16aaf984d03a8416
Hachounet <hachounet.github@gmx.fr>
2024-03-18 15:58:43 +0100

    Install Webpack/Dev-server/Webpack CLI
    Ignoring webpack.config for ESLint and Prettier

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  // 1
+  entry: './src/index.js',
+  // 2
+  output: {
+    path: '/dist',
+    filename: 'bundle.js',
+  },
+  // 3
+  devServer: {
+    static: './dist',
+  },
+};
